fb31b17066d623624ca55bc835e59edf3ea57898
Prabir Shrestha <mail@prabir.me>
2020-09-19 18:50:46 +0000

    add event-emitter

---
+++ README.md
@@ -0,0 +1,8 @@
+# EventEmitter for lua
+
+EventEmitter for lua extracted from [vis](https://github.com/martanne/vis).
+
+# LICENSE
+
+Refer to (vis MIT license](https://github.com/martanne/vis/blob/2290224c844775d446fc8aaf3a98c0149d935875/LICENSE)
+for more details
---
+++ event-emitter.lua
@@ -0,0 +1,60 @@
+local M = {}
+
+function M.new()
+    local eventemitter = {}
+
+    local handlers = {}
+
+    --- Subscribe to an event.
+    --
+    -- Register an event handler.
+    -- @tparam string event the event name
+    -- @tparam function handler the event handler
+    -- @tparam[opt] int index the index at which to insert the handler (1 is the highest priority)
+    eventemitter.on = function (event, handler, index)
+        if not event then error('Invalid event name') end
+        if type(handler) ~= 'function' then error('Invalid event handler') end
+        if not handlers[event] then handlers[event] = {} end
+        eventemitter.off(event, handler)
+        table.insert(handlers[event], index or #handlers[event] + 1, handler)
+    end
+
+    --- Unsubscribe from an event.
+    --
+    -- Remove a registered event handler.
+    -- @tparam string event the event name
+    -- @tparam function handler the event handler to unsubscribe
+    -- @treturn bool whether the handler was successfully removed
+    eventemitter.off = function (event, handler)
+        local h = handlers[event]
+        if not h then return end
+        for i = 1, #h do
+            if h[i] == handler then
+                table.remove(h, i)
+                return true
+            end
+        end
+        return false
+    end
+
+    --- Generate event.
+    --
+    -- Invokes all event handlers in the order they were registered.
+    -- Passes all arguments to the handler. The first handler which returns a non `nil`
+    -- value terminates the event propagation. The other handlers will not be called.
+    --
+    -- @tparam string event the event name
+    -- @tparam ... ... the remaining paramters are passed on to the handler
+    eventemitter.emit = function (event, ...)
+        local h = handlers[event]
+        if not h then return end
+        for i = 1, #h do
+            local ret = h[i](...)
+            if type(ret) ~= 'nil' then return ret end
+        end
+    end
+
+    return eventemitter
+end
+
+return M
